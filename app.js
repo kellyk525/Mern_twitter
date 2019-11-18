5f4518cf30124550e9ca1bbcf85ea544119836a6
Kelly <kjk224@cornell.edu>
2019-11-17 17:31:45 -0800

    Mern Twitter

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
-
-
 const express = require("express");
 const app = express();
+const db = require("./config/keys").mongoURI;
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
+const passport = require("passport");
 
-app.get("/", (req, res) => {
-    res.send("Hello a/A!");
-});
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
+
+
+// app.get("/", (req, res) => res.send("Hello World!!"));
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+// app.get("/", (req, res) => {
+//     const user = new User({
+//         handle: "Jim",
+//         email: "jim@gmail.com",
+//         password: "jimisgreat123"
+//     });
+//     user.save();
+//     console.log(res);
+//     res.send("Hello a/A!");
+// });
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 
